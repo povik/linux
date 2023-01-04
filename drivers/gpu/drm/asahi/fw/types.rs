lfnpcsoft/linux-asahi

drm/asahi: fw.types: Import GPUWeakPointer
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/fw/types.rs
@@ -7,7 +7,7 @@
 use crate::{alloc, object};
 pub(crate) use kernel::macros::versions;
 
-pub(crate) use crate::object::{GPUPointer, GPUStruct};
+pub(crate) use crate::object::{GPUPointer, GPUWeakPointer, GPUStruct};
 pub(crate) use ::alloc::boxed::Box;
 use core::fmt;
 use core::ops::{Deref, DerefMut, Index, IndexMut};

povik/linux

drivers/gpu/drm/asahi/fw/types.rs
@@ -7,7 +7,7 @@
 use crate::{alloc, object};
 pub(crate) use kernel::macros::versions;
 
-pub(crate) use crate::object::{GpuPointer, GpuStruct};
+pub(crate) use crate::object::{GpuPointer, GpuStruct, GpuWeakPointer};
 pub(crate) use ::alloc::boxed::Box;
 use core::fmt;
 use core::ops::{Deref, DerefMut, Index, IndexMut};