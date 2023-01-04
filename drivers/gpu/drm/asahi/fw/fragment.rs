lfnpcsoft/linux-asahi

drm/asahi: Wrap up vertex/fragment FW structures
Signed-off-by: Asahi Lina <lina@asahilina.net>

drivers/gpu/drm/asahi/fw/fragment.rs
@@ -1,0 +1,240 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+#![allow(dead_code)]
+
+//! GPU fragment jobs
+
+use super::types::*;
+use super::{event, job, workqueue};
+use crate::{buffer, fw, microseq};
+use kernel::sync::Ref;
+
+pub(crate) mod raw {
+    use super::*;
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub(crate) struct ClearPipelineBinding {
+        pub(crate) pipeline_bind: U64,
+        pub(crate) address: U64,
+    }
+
+    #[derive(Debug, Clone, Copy, Default)]
+    #[repr(C)]
+    pub(crate) struct StorePipelineBinding {
+        pub(crate) unk_0: U64,
+        pub(crate) unk_8: u32,
+        pub(crate) pipeline_bind: u32,
+        pub(crate) unk_10: u32,
+        pub(crate) address: u32,
+        pub(crate) unk_18: u32,
+        pub(crate) unk_1c_padding: u32,
+    }
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct ArrayAddr {
+        pub(crate) ptr: U64,
+        pub(crate) unk_padding: U64,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub(crate) struct AuxFBInfo {
+        pub(crate) unk1: u32,
+        pub(crate) unk2: u32,
+        pub(crate) width: u32,
+        pub(crate) height: u32,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk3: U64,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct JobParameters1 {
+        pub(crate) unk_0: U64,
+        pub(crate) clear_pipeline: ClearPipelineBinding,
+        pub(crate) unk_18: U64,
+        pub(crate) scissor_array: U64,
+        pub(crate) depth_bias_array: U64,
+        pub(crate) aux_fb: AuxFBInfo::ver,
+        pub(crate) depth_dimensions: U64,
+        pub(crate) unk_48: U64,
+        pub(crate) depth_flags: U64,
+        pub(crate) depth_buffer_ptr1: U64,
+        pub(crate) depth_buffer_ptr2: U64,
+        pub(crate) stencil_buffer_ptr1: U64,
+        pub(crate) stencil_buffer_ptr2: U64,
+        pub(crate) unk_68: Array<0xc, U64>,
+        pub(crate) tvb_tilemap: U64,
+        pub(crate) tvb_heapmeta_addr: U64,
+        pub(crate) unk_e8: U64,
+        pub(crate) tvb_heapmeta_addr2: U64,
+        pub(crate) unk_f8: U64,
+        pub(crate) aux_fb_ptr: U64,
+        pub(crate) unk_108: Array<0x6, U64>,
+        pub(crate) pipeline_base: U64,
+        pub(crate) unk_140: U64,
+        pub(crate) unk_148: U64,
+        pub(crate) unk_150: U64,
+        pub(crate) unk_158: U64,
+        pub(crate) unk_160_padding: Array<0x1e0, u8>,
+
+        #[ver(V < V13_0B4)]
+        pub(crate) __pad0: Pad<0x8>,
+    }
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct JobParameters2 {
+        pub(crate) store_pipeline_bind: u32,
+        pub(crate) store_pipeline_addr: u32,
+        pub(crate) unk_8: u32,
+        pub(crate) unk_c: u32,
+        pub(crate) uuid1: u32,
+        pub(crate) uuid2: u32,
+        pub(crate) unk_18: U64,
+        pub(crate) tile_blocks_y: u16,
+        pub(crate) tile_blocks_x: u16,
+        pub(crate) unk_24: u32,
+        pub(crate) tile_counts: u32,
+        pub(crate) unk_2c: u32,
+        pub(crate) depth_clear_val1: F32,
+        pub(crate) stencil_clear_val1: u8,
+        pub(crate) unk_35: u8,
+        pub(crate) unk_36: u16,
+        pub(crate) unk_38: u32,
+        pub(crate) unk_3c: u32,
+        pub(crate) unk_40: u32,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct JobParameters3 {
+        pub(crate) unk_40_padding: Array<0xac, u8>,
+        pub(crate) depth_bias_array: ArrayAddr,
+        pub(crate) scissor_array: ArrayAddr,
+        pub(crate) unk_110: U64,
+        pub(crate) unk_118: U64,
+        pub(crate) unk_120: [U64; 0x25],
+        pub(crate) unk_reload_pipeline: ClearPipelineBinding,
+        pub(crate) unk_258: U64,
+        pub(crate) unk_260: U64,
+        pub(crate) unk_268: U64,
+        pub(crate) unk_270: U64,
+        pub(crate) reload_pipeline: ClearPipelineBinding,
+        pub(crate) depth_flags: U64,
+        pub(crate) unk_290: U64,
+        pub(crate) depth_buffer_ptr1: U64,
+        pub(crate) unk_2a0: U64,
+        pub(crate) unk_2a8: U64,
+        pub(crate) depth_buffer_ptr2: U64,
+        pub(crate) depth_buffer_ptr3: U64,
+        pub(crate) unk_2c0: U64,
+        pub(crate) stencil_buffer_ptr1: U64,
+        pub(crate) unk_2d0: U64,
+        pub(crate) unk_2d8: U64,
+        pub(crate) stencil_buffer_ptr2: U64,
+        pub(crate) stencil_buffer_ptr3: U64,
+        pub(crate) unk_2f0: Array<3, U64>,
+        pub(crate) aux_fb_unk0: u32,
+        pub(crate) unk_30c: u32,
+        pub(crate) aux_fb: AuxFBInfo::ver,
+        pub(crate) unk_320_padding: Array<0x10, u8>,
+        pub(crate) unk_partial_store_pipeline: StorePipelineBinding,
+        pub(crate) partial_store_pipeline: StorePipelineBinding,
+        pub(crate) depth_clear_val2: F32,
+        pub(crate) stencil_clear_val2: u8,
+        pub(crate) unk_375: u8,
+        pub(crate) unk_376: u16,
+        pub(crate) unk_378: u32,
+        pub(crate) unk_37c: u32,
+        pub(crate) unk_380: U64,
+        pub(crate) unk_388: U64,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_390_0: U64,
+
+        pub(crate) depth_dimensions: U64,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct RunFragment<'a> {
+        pub(crate) tag: workqueue::CommandType,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) counter: U64,
+
+        pub(crate) vm_slot: u32,
+        pub(crate) unk_8: u32,
+        pub(crate) microsequence: GPUPointer<'a, &'a [u8]>,
+        pub(crate) microsequence_size: u32,
+        pub(crate) notifier: GPUPointer<'a, event::Notifier>,
+        pub(crate) buffer: GPUPointer<'a, fw::buffer::Info::ver>,
+        pub(crate) scene: GPUPointer<'a, fw::buffer::Scene::ver>,
+        pub(crate) unk_emptybuf_addr: U64,
+        pub(crate) tvb_tilemap: GPUPointer<'a, &'a [u8]>,
+        pub(crate) unk_40: U64,
+        pub(crate) unk_48: u32,
+        pub(crate) tile_blocks_y: u16,
+        pub(crate) tile_blocks_x: u16,
+        pub(crate) unk_50: U64,
+        pub(crate) unk_58: U64,
+        pub(crate) uuid1: u32,
+        pub(crate) uuid2: u32,
+        pub(crate) unk_68: U64,
+        pub(crate) tile_count: U64,
+        pub(crate) params_1: JobParameters1::ver,
+        pub(crate) params_2: JobParameters2,
+        pub(crate) params_3: JobParameters3::ver,
+        pub(crate) unk_758_flag: u32,
+        pub(crate) unk_75c_flag: u32,
+        pub(crate) unk_buf: Array<0x110, u8>,
+        pub(crate) busy_flag: u32,
+        pub(crate) tvb_overflow_count: u32,
+        pub(crate) unk_878: u32,
+        pub(crate) encoder_params: job::EncoderParams,
+        pub(crate) unk_pointee: u32,
+        pub(crate) meta: job::JobMeta,
+        pub(crate) unk_914: u32,
+        pub(crate) unk_918: U64,
+        pub(crate) unk_920: u32,
+        pub(crate) unk_924: u32,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_928_0: u32,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_928_4: u8,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) ts_flag: u8,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_5e6: u16,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_5e8: Array<0x20, u8>,
+        // Alignment - handled by allocator
+        //pad_928: [u8; 0x18],
+    }
+}
+
+#[versions(AGX)]
+#[derive(Debug)]
+pub(crate) struct RunFragment {
+    pub(crate) notifier: Ref<GPUObject<event::Notifier>>,
+    pub(crate) scene: Ref<buffer::Scene::ver>,
+    pub(crate) micro_seq: microseq::MicroSequence,
+}
+
+#[versions(AGX)]
+impl GPUStruct for RunFragment::ver {
+    type Raw<'a> = raw::RunFragment::ver<'a>;
+}

drivers/gpu/drm/asahi/fw/microseq.rs
@@ -5,7 +5,7 @@
 //! GPU events & stamps
 
 use super::types::*;
-use super::{buffer, initdata, vertex, workqueue};
+use super::{buffer, fragment, initdata, vertex, workqueue};
 
 pub(crate) trait Operation {}
 
@@ -86,41 +86,41 @@ pub(crate) mod op {
 #[derive(Debug)]
 #[repr(C)]
 struct StartVertex<'a> {
-    header: op::StartVertex,
-    tiling_params: GPUWeakPointer<vertex::raw::TilingParameters>,
-    job_params: GPUWeakPointer<vertex::raw::JobParameters<'a>>,
-    buffer: GPUWeakPointer<buffer::Info::ver>,
-    scene: GPUWeakPointer<buffer::Scene::ver>,
-    stats_ptr: GPUWeakPointer<initdata::raw::GPUStatsVtx::ver>,
-    work_queue: GPUWeakPointer<workqueue::QueueInfo>,
-    vm_slot: u32,
-    unk_38: u32,
-    event_generation: u32,
-    buffer_slot: u64,
-    unk_48: u64,
-    unk_50: u32,
-    job_meta: GPUWeakPointer<vertex::raw::JobMeta<'a>>,
-    unk_job_buf: GPUWeakPointer<Array<0x18, u8>>,
-    unk_64: u32,
-    unk_68: u32,
-    uuid: u32,
-    unk_70: u32,
-    unk_74: Array<0x1d, u64>,
-    unk_15c: u32,
-    unk_160: u64,
-    unk_168: u32,
-    unk_16c: u32,
-    unk_170: u64,
-    unk_178: u32,
+    pub(crate) header: op::StartVertex,
+    pub(crate) tiling_params: GPUWeakPointer<vertex::raw::TilingParameters>,
+    pub(crate) job_params: GPUWeakPointer<vertex::raw::JobParameters1<'a>>,
+    pub(crate) buffer: GPUWeakPointer<buffer::Info::ver>,
+    pub(crate) scene: GPUWeakPointer<buffer::Scene::ver>,
+    pub(crate) stats_ptr: GPUWeakPointer<initdata::raw::GPUStatsVtx::ver>,
+    pub(crate) work_queue: GPUWeakPointer<workqueue::QueueInfo>,
+    pub(crate) vm_slot: u32,
+    pub(crate) unk_38: u32,
+    pub(crate) event_generation: u32,
+    pub(crate) buffer_slot: u64,
+    pub(crate) unk_48: u64,
+    pub(crate) unk_50: u32,
+    pub(crate) unk_pointer: GPUWeakPointer<u32>,
+    pub(crate) unk_job_buf: GPUWeakPointer<Array<0x18, u8>>,
+    pub(crate) unk_64: u32,
+    pub(crate) unk_68: u32,
+    pub(crate) uuid: u32,
+    pub(crate) unk_70: u32,
+    pub(crate) unk_74: Array<0x1d, u64>,
+    pub(crate) unk_15c: u32,
+    pub(crate) unk_160: u64,
+    pub(crate) unk_168: u32,
+    pub(crate) unk_16c: u32,
+    pub(crate) unk_170: u64,
+    pub(crate) unk_178: u32,
 
     #[ver(V >= V13_0B4)]
-    unk_17c: u32,
+    pub(crate) unk_17c: u32,
 
     #[ver(V >= V13_0B4)]
-    notifier_buf: GPUWeakPointer<Array<0x8, u8>>,
+    pub(crate) notifier_buf: GPUWeakPointer<Array<0x8, u8>>,
 
     #[ver(V >= V13_0B4)]
-    unk_188: u32,
+    pub(crate) unk_188: u32,
 }
 
 #[versions(AGX)]
@@ -129,32 +129,117 @@ impl<'a> Operation for StartVertex::ver<'a> {}
 #[versions(AGX)]
 #[derive(Debug)]
 #[repr(C)]
-struct FinalizeVertex<'a> {
-    opcode: u32,
-    scene: GPUWeakPointer<buffer::Scene::ver>,
-    buffer: GPUWeakPointer<buffer::Info::ver>,
-    stats_ptr: GPUWeakPointer<initdata::raw::GPUStatsVtx::ver>,
-    work_queue: GPUWeakPointer<workqueue::QueueInfo>,
-    vm_slot: u32,
-    unk_28: u32,
-    job_meta: GPUWeakPointer<vertex::raw::JobMeta<'a>>,
-    unk_34: u32,
-    uuid: u32,
-    fw_stamp: GPUWeakPointer<FWStamp>,
-    stamp_value: u32,
-    unk_48: u64,
-    unk_50: u32,
-    unk_54: u32,
-    unk_58: U64,
-    unk_60: u32,
-    unk_64: u32,
-    unk_68: u32,
-    restart_branch_offset: i32,
-    unk_70: u32,
+struct FinalizeVertex {
+    pub(crate) opcode: op::FinalizeVertex,
+    pub(crate) scene: GPUWeakPointer<buffer::Scene::ver>,
+    pub(crate) buffer: GPUWeakPointer<buffer::Info::ver>,
+    pub(crate) stats_ptr: GPUWeakPointer<initdata::raw::GPUStatsVtx::ver>,
+    pub(crate) work_queue: GPUWeakPointer<workqueue::QueueInfo>,
+    pub(crate) vm_slot: u32,
+    pub(crate) unk_28: u32,
+    pub(crate) unk_pointer: GPUWeakPointer<u32>,
+    pub(crate) unk_34: u32,
+    pub(crate) uuid: u32,
+    pub(crate) fw_stamp: GPUWeakPointer<FWStamp>,
+    pub(crate) stamp_value: u32,
+    pub(crate) unk_48: u64,
+    pub(crate) unk_50: u32,
+    pub(crate) unk_54: u32,
+    pub(crate) unk_58: U64,
+    pub(crate) unk_60: u32,
+    pub(crate) unk_64: u32,
+    pub(crate) unk_68: u32,
+    pub(crate) restart_branch_offset: i32,
+    pub(crate) unk_70: u32,
 
     #[ver(V >= V13_0B4)]
-    unk_74: Array<0x10, u8>,
+    pub(crate) unk_74: Array<0x10, u8>,
 }
 
 #[versions(AGX)]
-impl<'a> Operation for FinalizeVertex::ver<'a> {}
+impl Operation for FinalizeVertex::ver {}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+pub(crate) struct Attachment {
+    pub(crate) address: U64,
+    pub(crate) size: u32,
+    pub(crate) unk_c: u16,
+    pub(crate) unk_e: u16,
+}
+
+#[versions(AGX)]
+#[derive(Debug)]
+#[repr(C)]
+struct StartFragment<'a> {
+    pub(crate) opcode: op::StartFragment,
+    pub(crate) job_params2: U64,
+    pub(crate) job_params1: U64,
+    pub(crate) scene: GPUPointer<'a, buffer::Scene::ver>,
+    pub(crate) stats_ptr: U64,
+    pub(crate) busy_flag_ptr: GPUWeakPointer<u32>,
+    pub(crate) tvb_overflow_count: GPUWeakPointer<u32>,
+    pub(crate) unk_pointer: GPUWeakPointer<u32>,
+    pub(crate) cmdqueue_ptr: U64,
+    pub(crate) workitem_ptr: U64,
+    pub(crate) context_id: u32,
+    pub(crate) unk_50: u32,
+    pub(crate) event_generation: u32,
+    pub(crate) buffer_mgr_slot: u32,
+    pub(crate) unk_5c: u32,
+    pub(crate) prev_stamp_value: U64,
+    pub(crate) unk_68: u32,
+    pub(crate) unk_buf_ptr: U64,
+    pub(crate) unk_buf2_ptr: U64,
+    pub(crate) unk_7c: u32,
+    pub(crate) unk_80: u32,
+    pub(crate) unk_84: u32,
+    pub(crate) uuid: u32,
+    pub(crate) attachments: Array<0x10, Attachment>,
+    pub(crate) num_attachments: u32,
+    pub(crate) unk_190: u32,
+
+    #[ver(V >= V13_0B4)]
+    pub(crate) unk_194: U64,
+
+    #[ver(V >= V13_0B4)]
+    pub(crate) unkptr_19c: U64,
+}
+
+#[versions(AGX)]
+impl<'a> Operation for StartFragment::ver<'a> {}
+
+#[versions(AGX)]
+#[derive(Debug)]
+#[repr(C)]
+struct FinalizeFragment {
+    pub(crate) opcode: op::FinalizeFragment,
+    pub(crate) uuid: u32,
+    pub(crate) unk_8: u32,
+    pub(crate) fw_stamp: GPUWeakPointer<FWStamp>,
+    pub(crate) stamp_value: u32,
+    pub(crate) unk_18: u32,
+    pub(crate) scene: GPUWeakPointer<buffer::Scene::ver>,
+    pub(crate) buffer: GPUWeakPointer<buffer::Info::ver>,
+    pub(crate) unk_2c: U64,
+    pub(crate) stats_ptr: GPUWeakPointer<initdata::raw::GPUStatsFrag::ver>,
+    pub(crate) unk_pointer: GPUWeakPointer<u32>,
+    pub(crate) busy_flag: GPUWeakPointer<u32>,
+    pub(crate) work_queue: GPUWeakPointer<workqueue::QueueInfo>,
+    pub(crate) work_item: GPUWeakPointer<fragment::RunFragment::ver>,
+    pub(crate) unk_5c: U64,
+    pub(crate) unk_758_ptr: GPUWeakPointer<u32>,
+    pub(crate) unk_6c: U64,
+    pub(crate) unk_74: U64,
+    pub(crate) unk_7c: U64,
+    pub(crate) unk_84: U64,
+    pub(crate) unk_8c: U64,
+    pub(crate) restart_branch_offset: i32,
+    pub(crate) unk_98: u32,
+
+    #[ver(V >= V13_0B4)]
+    pub(crate) unk_9c: Array<0x10, u8>,
+}
+
+#[versions(AGX)]
+impl<'a> Operation for FinalizeFragment::ver {}

povik/linux

drivers/gpu/drm/asahi/fw/fragment.rs
@@ -1,0 +1,240 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+#![allow(missing_docs)]
+#![allow(dead_code)]
+
+//! GPU fragment jobs
+
+use super::types::*;
+use super::{event, job, workqueue};
+use crate::{buffer, fw, microseq};
+use kernel::sync::Arc;
+
+pub(crate) mod raw {
+    use super::*;
+
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub(crate) struct ClearPipelineBinding {
+        pub(crate) pipeline_bind: U64,
+        pub(crate) address: U64,
+    }
+
+    #[derive(Debug, Clone, Copy, Default)]
+    #[repr(C)]
+    pub(crate) struct StorePipelineBinding {
+        pub(crate) unk_0: U64,
+        pub(crate) unk_8: u32,
+        pub(crate) pipeline_bind: u32,
+        pub(crate) unk_10: u32,
+        pub(crate) address: u32,
+        pub(crate) unk_18: u32,
+        pub(crate) unk_1c_padding: u32,
+    }
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct ArrayAddr {
+        pub(crate) ptr: U64,
+        pub(crate) unk_padding: U64,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug, Clone, Copy)]
+    #[repr(C)]
+    pub(crate) struct AuxFBInfo {
+        pub(crate) unk1: u32,
+        pub(crate) unk2: u32,
+        pub(crate) width: u32,
+        pub(crate) height: u32,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk3: U64,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct JobParameters1 {
+        pub(crate) unk_0: U64,
+        pub(crate) clear_pipeline: ClearPipelineBinding,
+        pub(crate) unk_18: U64,
+        pub(crate) scissor_array: U64,
+        pub(crate) depth_bias_array: U64,
+        pub(crate) aux_fb: AuxFBInfo::ver,
+        pub(crate) depth_dimensions: U64,
+        pub(crate) unk_48: U64,
+        pub(crate) depth_flags: U64,
+        pub(crate) depth_buffer_ptr1: U64,
+        pub(crate) depth_buffer_ptr2: U64,
+        pub(crate) stencil_buffer_ptr1: U64,
+        pub(crate) stencil_buffer_ptr2: U64,
+        pub(crate) unk_68: Array<0xc, U64>,
+        pub(crate) tvb_tilemap: U64,
+        pub(crate) tvb_heapmeta_addr: U64,
+        pub(crate) unk_e8: U64,
+        pub(crate) tvb_heapmeta_addr2: U64,
+        pub(crate) unk_f8: U64,
+        pub(crate) aux_fb_ptr: U64,
+        pub(crate) unk_108: Array<0x6, U64>,
+        pub(crate) pipeline_base: U64,
+        pub(crate) unk_140: U64,
+        pub(crate) unk_148: U64,
+        pub(crate) unk_150: U64,
+        pub(crate) unk_158: U64,
+        pub(crate) unk_160_padding: Array<0x1e0, u8>,
+
+        #[ver(V < V13_0B4)]
+        pub(crate) __pad0: Pad<0x8>,
+    }
+
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct JobParameters2 {
+        pub(crate) store_pipeline_bind: u32,
+        pub(crate) store_pipeline_addr: u32,
+        pub(crate) unk_8: u32,
+        pub(crate) unk_c: u32,
+        pub(crate) uuid1: u32,
+        pub(crate) uuid2: u32,
+        pub(crate) unk_18: U64,
+        pub(crate) tile_blocks_y: u16,
+        pub(crate) tile_blocks_x: u16,
+        pub(crate) unk_24: u32,
+        pub(crate) tile_counts: u32,
+        pub(crate) unk_2c: u32,
+        pub(crate) depth_clear_val1: F32,
+        pub(crate) stencil_clear_val1: u8,
+        pub(crate) unk_35: u8,
+        pub(crate) unk_36: u16,
+        pub(crate) unk_38: u32,
+        pub(crate) unk_3c: u32,
+        pub(crate) unk_40: u32,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct JobParameters3 {
+        pub(crate) unk_40_padding: Array<0xac, u8>,
+        pub(crate) depth_bias_array: ArrayAddr,
+        pub(crate) scissor_array: ArrayAddr,
+        pub(crate) unk_110: U64,
+        pub(crate) unk_118: U64,
+        pub(crate) unk_120: [U64; 0x25],
+        pub(crate) unk_reload_pipeline: ClearPipelineBinding,
+        pub(crate) unk_258: U64,
+        pub(crate) unk_260: U64,
+        pub(crate) unk_268: U64,
+        pub(crate) unk_270: U64,
+        pub(crate) reload_pipeline: ClearPipelineBinding,
+        pub(crate) depth_flags: U64,
+        pub(crate) unk_290: U64,
+        pub(crate) depth_buffer_ptr1: U64,
+        pub(crate) unk_2a0: U64,
+        pub(crate) unk_2a8: U64,
+        pub(crate) depth_buffer_ptr2: U64,
+        pub(crate) depth_buffer_ptr3: U64,
+        pub(crate) unk_2c0: U64,
+        pub(crate) stencil_buffer_ptr1: U64,
+        pub(crate) unk_2d0: U64,
+        pub(crate) unk_2d8: U64,
+        pub(crate) stencil_buffer_ptr2: U64,
+        pub(crate) stencil_buffer_ptr3: U64,
+        pub(crate) unk_2f0: Array<3, U64>,
+        pub(crate) aux_fb_unk0: u32,
+        pub(crate) unk_30c: u32,
+        pub(crate) aux_fb: AuxFBInfo::ver,
+        pub(crate) unk_320_padding: Array<0x10, u8>,
+        pub(crate) unk_partial_store_pipeline: StorePipelineBinding,
+        pub(crate) partial_store_pipeline: StorePipelineBinding,
+        pub(crate) depth_clear_val2: F32,
+        pub(crate) stencil_clear_val2: u8,
+        pub(crate) unk_375: u8,
+        pub(crate) unk_376: u16,
+        pub(crate) unk_378: u32,
+        pub(crate) unk_37c: u32,
+        pub(crate) unk_380: U64,
+        pub(crate) unk_388: U64,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_390_0: U64,
+
+        pub(crate) depth_dimensions: U64,
+    }
+
+    #[versions(AGX)]
+    #[derive(Debug)]
+    #[repr(C)]
+    pub(crate) struct RunFragment<'a> {
+        pub(crate) tag: workqueue::CommandType,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) counter: U64,
+
+        pub(crate) vm_slot: u32,
+        pub(crate) unk_8: u32,
+        pub(crate) microsequence: GpuPointer<'a, &'a [u8]>,
+        pub(crate) microsequence_size: u32,
+        pub(crate) notifier: GpuPointer<'a, event::Notifier>,
+        pub(crate) buffer: GpuPointer<'a, fw::buffer::Info::ver>,
+        pub(crate) scene: GpuPointer<'a, fw::buffer::Scene::ver>,
+        pub(crate) unk_emptybuf_addr: U64,
+        pub(crate) tvb_tilemap: GpuPointer<'a, &'a [u8]>,
+        pub(crate) unk_40: U64,
+        pub(crate) unk_48: u32,
+        pub(crate) tile_blocks_y: u16,
+        pub(crate) tile_blocks_x: u16,
+        pub(crate) unk_50: U64,
+        pub(crate) unk_58: U64,
+        pub(crate) uuid1: u32,
+        pub(crate) uuid2: u32,
+        pub(crate) unk_68: U64,
+        pub(crate) tile_count: U64,
+        pub(crate) params_1: JobParameters1::ver,
+        pub(crate) params_2: JobParameters2,
+        pub(crate) params_3: JobParameters3::ver,
+        pub(crate) unk_758_flag: u32,
+        pub(crate) unk_75c_flag: u32,
+        pub(crate) unk_buf: Array<0x110, u8>,
+        pub(crate) busy_flag: u32,
+        pub(crate) tvb_overflow_count: u32,
+        pub(crate) unk_878: u32,
+        pub(crate) encoder_params: job::EncoderParams,
+        pub(crate) unk_pointee: u32,
+        pub(crate) meta: job::JobMeta,
+        pub(crate) unk_914: u32,
+        pub(crate) unk_918: U64,
+        pub(crate) unk_920: u32,
+        pub(crate) unk_924: u32,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_928_0: u32,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_928_4: u8,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) ts_flag: u8,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_5e6: u16,
+
+        #[ver(V >= V13_0B4)]
+        pub(crate) unk_5e8: Array<0x20, u8>,
+        // Alignment - handled by allocator
+        //pad_928: [u8; 0x18],
+    }
+}
+
+#[versions(AGX)]
+#[derive(Debug)]
+pub(crate) struct RunFragment {
+    pub(crate) notifier: Arc<GpuObject<event::Notifier>>,
+    pub(crate) scene: Arc<buffer::Scene::ver>,
+    pub(crate) micro_seq: microseq::MicroSequence,
+}
+
+#[versions(AGX)]
+impl GpuStruct for RunFragment::ver {
+    type Raw<'a> = raw::RunFragment::ver<'a>;
+}

drivers/gpu/drm/asahi/fw/microseq.rs
@@ -5,7 +5,7 @@
 //! GPU events & stamps
 
 use super::types::*;
-use super::{buffer, initdata, vertex, workqueue};
+use super::{buffer, fragment, initdata, vertex, workqueue};
 
 pub(crate) trait Operation {}
 
@@ -86,41 +86,41 @@ pub(crate) mod op {
 #[derive(Debug)]
 #[repr(C)]
 struct StartVertex<'a> {
-    header: op::StartVertex,
-    tiling_params: GpuWeakPointer<vertex::raw::TilingParameters>,
-    job_params: GpuWeakPointer<vertex::raw::JobParameters<'a>>,
-    buffer: GpuWeakPointer<buffer::Info::ver>,
-    scene: GpuWeakPointer<buffer::Scene::ver>,
-    stats_ptr: GpuWeakPointer<initdata::raw::GpuStatsVtx::ver>,
-    work_queue: GpuWeakPointer<workqueue::QueueInfo>,
-    vm_slot: u32,
-    unk_38: u32,
-    event_generation: u32,
-    buffer_slot: u64,
-    unk_48: u64,
-    unk_50: u32,
-    job_meta: GpuWeakPointer<vertex::raw::JobMeta<'a>>,
-    unk_job_buf: GpuWeakPointer<Array<0x18, u8>>,
-    unk_64: u32,
-    unk_68: u32,
-    uuid: u32,
-    unk_70: u32,
-    unk_74: Array<0x1d, u64>,
-    unk_15c: u32,
-    unk_160: u64,
-    unk_168: u32,
-    unk_16c: u32,
-    unk_170: u64,
-    unk_178: u32,
+    pub(crate) header: op::StartVertex,
+    pub(crate) tiling_params: GpuWeakPointer<vertex::raw::TilingParameters>,
+    pub(crate) job_params: GpuWeakPointer<vertex::raw::JobParameters1<'a>>,
+    pub(crate) buffer: GpuWeakPointer<buffer::Info::ver>,
+    pub(crate) scene: GpuWeakPointer<buffer::Scene::ver>,
+    pub(crate) stats_ptr: GpuWeakPointer<initdata::raw::GpuStatsVtx::ver>,
+    pub(crate) work_queue: GpuWeakPointer<workqueue::QueueInfo>,
+    pub(crate) vm_slot: u32,
+    pub(crate) unk_38: u32,
+    pub(crate) event_generation: u32,
+    pub(crate) buffer_slot: u64,
+    pub(crate) unk_48: u64,
+    pub(crate) unk_50: u32,
+    pub(crate) unk_pointer: GpuWeakPointer<u32>,
+    pub(crate) unk_job_buf: GpuWeakPointer<Array<0x18, u8>>,
+    pub(crate) unk_64: u32,
+    pub(crate) unk_68: u32,
+    pub(crate) uuid: u32,
+    pub(crate) unk_70: u32,
+    pub(crate) unk_74: Array<0x1d, u64>,
+    pub(crate) unk_15c: u32,
+    pub(crate) unk_160: u64,
+    pub(crate) unk_168: u32,
+    pub(crate) unk_16c: u32,
+    pub(crate) unk_170: u64,
+    pub(crate) unk_178: u32,
 
     #[ver(V >= V13_0B4)]
-    unk_17c: u32,
+    pub(crate) unk_17c: u32,
 
     #[ver(V >= V13_0B4)]
-    notifier_buf: GpuWeakPointer<Array<0x8, u8>>,
+    pub(crate) notifier_buf: GpuWeakPointer<Array<0x8, u8>>,
 
     #[ver(V >= V13_0B4)]
-    unk_188: u32,
+    pub(crate) unk_188: u32,
 }
 
 #[versions(AGX)]
@@ -129,32 +129,117 @@ impl<'a> Operation for StartVertex::ver<'a> {}
 #[versions(AGX)]
 #[derive(Debug)]
 #[repr(C)]
-struct FinalizeVertex<'a> {
-    opcode: u32,
-    scene: GpuWeakPointer<buffer::Scene::ver>,
-    buffer: GpuWeakPointer<buffer::Info::ver>,
-    stats_ptr: GpuWeakPointer<initdata::raw::GpuStatsVtx::ver>,
-    work_queue: GpuWeakPointer<workqueue::QueueInfo>,
-    vm_slot: u32,
-    unk_28: u32,
-    job_meta: GpuWeakPointer<vertex::raw::JobMeta<'a>>,
-    unk_34: u32,
-    uuid: u32,
-    fw_stamp: GpuWeakPointer<FwStamp>,
-    stamp_value: u32,
-    unk_48: u64,
-    unk_50: u32,
-    unk_54: u32,
-    unk_58: U64,
-    unk_60: u32,
-    unk_64: u32,
-    unk_68: u32,
-    restart_branch_offset: i32,
-    unk_70: u32,
+struct FinalizeVertex {
+    pub(crate) opcode: op::FinalizeVertex,
+    pub(crate) scene: GpuWeakPointer<buffer::Scene::ver>,
+    pub(crate) buffer: GpuWeakPointer<buffer::Info::ver>,
+    pub(crate) stats_ptr: GpuWeakPointer<initdata::raw::GpuStatsVtx::ver>,
+    pub(crate) work_queue: GpuWeakPointer<workqueue::QueueInfo>,
+    pub(crate) vm_slot: u32,
+    pub(crate) unk_28: u32,
+    pub(crate) unk_pointer: GpuWeakPointer<u32>,
+    pub(crate) unk_34: u32,
+    pub(crate) uuid: u32,
+    pub(crate) fw_stamp: GpuWeakPointer<FwStamp>,
+    pub(crate) stamp_value: u32,
+    pub(crate) unk_48: u64,
+    pub(crate) unk_50: u32,
+    pub(crate) unk_54: u32,
+    pub(crate) unk_58: U64,
+    pub(crate) unk_60: u32,
+    pub(crate) unk_64: u32,
+    pub(crate) unk_68: u32,
+    pub(crate) restart_branch_offset: i32,
+    pub(crate) unk_70: u32,
 
     #[ver(V >= V13_0B4)]
-    unk_74: Array<0x10, u8>,
+    pub(crate) unk_74: Array<0x10, u8>,
 }
 
 #[versions(AGX)]
-impl<'a> Operation for FinalizeVertex::ver<'a> {}
+impl Operation for FinalizeVertex::ver {}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+pub(crate) struct Attachment {
+    pub(crate) address: U64,
+    pub(crate) size: u32,
+    pub(crate) unk_c: u16,
+    pub(crate) unk_e: u16,
+}
+
+#[versions(AGX)]
+#[derive(Debug)]
+#[repr(C)]
+struct StartFragment<'a> {
+    pub(crate) opcode: op::StartFragment,
+    pub(crate) job_params2: U64,
+    pub(crate) job_params1: U64,
+    pub(crate) scene: GpuPointer<'a, buffer::Scene::ver>,
+    pub(crate) stats_ptr: U64,
+    pub(crate) busy_flag_ptr: GpuWeakPointer<u32>,
+    pub(crate) tvb_overflow_count: GpuWeakPointer<u32>,
+    pub(crate) unk_pointer: GpuWeakPointer<u32>,
+    pub(crate) cmdqueue_ptr: U64,
+    pub(crate) workitem_ptr: U64,
+    pub(crate) context_id: u32,
+    pub(crate) unk_50: u32,
+    pub(crate) event_generation: u32,
+    pub(crate) buffer_mgr_slot: u32,
+    pub(crate) unk_5c: u32,
+    pub(crate) prev_stamp_value: U64,
+    pub(crate) unk_68: u32,
+    pub(crate) unk_buf_ptr: U64,
+    pub(crate) unk_buf2_ptr: U64,
+    pub(crate) unk_7c: u32,
+    pub(crate) unk_80: u32,
+    pub(crate) unk_84: u32,
+    pub(crate) uuid: u32,
+    pub(crate) attachments: Array<0x10, Attachment>,
+    pub(crate) num_attachments: u32,
+    pub(crate) unk_190: u32,
+
+    #[ver(V >= V13_0B4)]
+    pub(crate) unk_194: U64,
+
+    #[ver(V >= V13_0B4)]
+    pub(crate) unkptr_19c: U64,
+}
+
+#[versions(AGX)]
+impl<'a> Operation for StartFragment::ver<'a> {}
+
+#[versions(AGX)]
+#[derive(Debug)]
+#[repr(C)]
+struct FinalizeFragment {
+    pub(crate) opcode: op::FinalizeFragment,
+    pub(crate) uuid: u32,
+    pub(crate) unk_8: u32,
+    pub(crate) fw_stamp: GpuWeakPointer<FwStamp>,
+    pub(crate) stamp_value: u32,
+    pub(crate) unk_18: u32,
+    pub(crate) scene: GpuWeakPointer<buffer::Scene::ver>,
+    pub(crate) buffer: GpuWeakPointer<buffer::Info::ver>,
+    pub(crate) unk_2c: U64,
+    pub(crate) stats_ptr: GpuWeakPointer<initdata::raw::GpuStatsFrag::ver>,
+    pub(crate) unk_pointer: GpuWeakPointer<u32>,
+    pub(crate) busy_flag: GpuWeakPointer<u32>,
+    pub(crate) work_queue: GpuWeakPointer<workqueue::QueueInfo>,
+    pub(crate) work_item: GpuWeakPointer<fragment::RunFragment::ver>,
+    pub(crate) unk_5c: U64,
+    pub(crate) unk_758_ptr: GpuWeakPointer<u32>,
+    pub(crate) unk_6c: U64,
+    pub(crate) unk_74: U64,
+    pub(crate) unk_7c: U64,
+    pub(crate) unk_84: U64,
+    pub(crate) unk_8c: U64,
+    pub(crate) restart_branch_offset: i32,
+    pub(crate) unk_98: u32,
+
+    #[ver(V >= V13_0B4)]
+    pub(crate) unk_9c: Array<0x10, u8>,
+}
+
+#[versions(AGX)]
+impl Operation for FinalizeFragment::ver {}