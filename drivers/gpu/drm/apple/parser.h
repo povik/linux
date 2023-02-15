6c976e3732ead2f1efa3582464bd9bd3ddb1b94b
Martin Povišer <povik+lin@cutebit.org>
2023-02-15 16:22:17 +0100

    gpu: drm: apple: Add sound mode parsing
    Signed-off-by: Martin Povišer <povik+lin@cutebit.org>

--- drivers/gpu/drm/apple/parser.h
+++ drivers/gpu/drm/apple/parser.h
@@ -33,4 +33,24 @@ int parse_display_attributes(struct dcp_parse_ctx *handle, int *width_mm,
 			     int *height_mm);
 int parse_epic_service_init(struct dcp_parse_ctx *handle, const char **name,
 			    const char **class, s64 *unit);
+
+struct dcp_sound_format_mask {
+	u64 formats;			/* SNDRV_PCM_FMTBIT_* */
+	unsigned int rates;		/* SNDRV_PCM_RATE_* */
+	unsigned int nchans;
+};
+
+struct dcp_sound_cookie {
+	u8 data[24];
+};
+
+struct snd_pcm_chmap_elem;
+int parse_sound_constraints(struct dcp_parse_ctx *handle,
+			    struct dcp_sound_format_mask *sieve,
+			    struct dcp_sound_format_mask *hits);
+int parse_sound_mode(struct dcp_parse_ctx *handle,
+		     struct dcp_sound_format_mask *sieve,
+		     struct snd_pcm_chmap_elem *chmap,
+		     struct dcp_sound_cookie *cookie);
+
 #endif
